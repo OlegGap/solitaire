2231bcc165e9a169fc0e48956111d4df82ade7f5
oleg <hnapovskiy20@gmail.com>
2019-09-14 20:07:51 +0300

    Updates

---
+++ precache-manifest.c9aa60d6164de1c4ee79120bcc9c0884.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8111c90182668f5573fdd7a6fab159e",
+    "url": "/solitaire/index.html"
+  },
+  {
+    "revision": "f339dbd8084efaff0e46",
+    "url": "/solitaire/static/js/2.16d5a38c.chunk.js"
+  },
+  {
+    "revision": "4b63ef32e874b97cdcb4",
+    "url": "/solitaire/static/js/main.40613d89.chunk.js"
+  },
+  {
+    "revision": "150f9d6c3b59e42eb2fb",
+    "url": "/solitaire/static/js/runtime~main.6725377d.js"
+  }
+]);
