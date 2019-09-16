64bd5613a151168ef9060fb1b571ff2b074a29c0
oleg <hnapovskiy20@gmail.com>
2019-09-16 16:17:02 +0300

    add header

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
-import Conditioner from "./components/Conditioner";
-import Timer from "./components/Timer"; 
-import HoverTabel from "./components/hoverTable/index.js";
+import App from "./App";
+import Conditioner from "./components_old/Conditioner";
+import Timer from "./components_old/Timer"; 
+import HoverTabel from "./components_old/hoverTable/index.js";
 
 
 ReactDOM.render(
@@ -11,8 +11,9 @@ ReactDOM.render(
     {/* <App name="Oleg" />
     <Conditioner />
     <p></p>
-    <Timer /> */}
-   <HoverTabel />
+    <Timer />
+   <HoverTabel /> */}
+   <App />
   </Fragment>,
   document.getElementById("root")
 );
